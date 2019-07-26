d4f30ea43428e7a31bc37d7d6d02f7cfbb329c31
Aryan Kumar <kumar.aryan@gmail.com>
2019-07-25 23:27:08 -0700

    Update lister.java

--- cDriveLister/src/lister.java
+++ cDriveLister/src/lister.java
@@ -11,22 +11,40 @@ public class lister {
     }
 
     private void list(File file, String indent){
+        
+        // a try catch block to ensure that the program doesn't break when it encounters an admin only folder
+        
         try {
+            
+            // checks if the current file is a directory
+            
             if (file.isDirectory()) {
+                
+                // creates an array of all the files in the directory
+                
                 File[] list = file.listFiles();
+                
+                // prints out the directory name
+                
                 System.out.println(indent + file.getName());
+                
+                // goes through each file in the directory then lists each of them with an extra indent 
+                
                 for (File f : list) {
                     list(f, indent + "\t");
                 }
             } else {
+                
+                // if the file isn't a directory it just prints the name of the file with the indent
+                
                 System.out.println(indent + file.getName());
             }
-        }catch (NullPointerException n){
+        } catch (NullPointerException n) {
             System.out.println(file.getName() + "has admin only access");
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         File f = new File("C:\\"); //this can be replaced with any desired location
         lister lister = new lister(f);
         lister.list(f,"");
